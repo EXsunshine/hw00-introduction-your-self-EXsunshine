bf1d632d5173d3d38bff99aa7ed371b3819bbac6
EXsunshine <105021057@live.asia.edu.tw>
2016-09-19 15:47:44 +0800

    EX OK

---
+++ test/src/test.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner scn = new Scanner(System.in);
+int a = scn.nextInt();
+int c =(a-32)*5/9;
+System.out.println(c);
+	}
+
+}
